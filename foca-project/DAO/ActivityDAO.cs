9249ba681adf528217ac82da5b5c788dd2cfd229
matheusjardim-dev <matheusjardim.dev@gmail.com>
2024-06-04 12:08:40 -0300

    UD tasks

--- foca-project/DAO/ActivityDAO.cs
+++ foca-project/DAO/ActivityDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using foca_project.Models;
+using foca_project.Views;
 using MySql.Data.MySqlClient;
 
 namespace foca_project.DAO
@@ -36,11 +37,60 @@ namespace foca_project.DAO
 
         public void CreateActivity(ActivityModel activity)
         {
-            string[] properties = { "title", "description", "date_init", "date_end", "category_idcategory", "users_idusers" };
-            string[] values = { activity.Title, activity.Description, activity.Date_init.ToString("yyyy-MM-dd"), activity.Date_end.ToString("yyyy-MM-dd"), "1", activity.Id_user.ToString() };
-
+            string[] properties = { 
+                "title", 
+                "description", 
+                "date_init", 
+                "date_end", 
+                "category_idcategory", 
+                "users_idusers" 
+            };
+            string[] values = { 
+                activity.Title, 
+                activity.Description, 
+                activity.Date_init.ToString("yyyy-MM-dd"), 
+                activity.Date_end.ToString("yyyy-MM-dd"), 
+                "1", 
+                activity.Id_user.ToString()
+            };
+           
             MySqlCommand cmd = _CRUDDAO.Create("tasks", properties, values);
             cmd.ExecuteNonQuery();
+        }
+
+        public ActivityModel GetActivityById(int idActivity)
+        {
+            string[] where = { "idtasks = " + idActivity.ToString() };
+            MySqlCommand cmd = _CRUDDAO.SelectById("tasks", where);
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    return new ActivityModel
+                    {
+                        Id = Convert.ToInt32(reader["idtasks"]),
+                        Title = reader["title"].ToString(),
+                        Description = reader["description"].ToString(),
+                        Date_end = Convert.ToDateTime(reader["date_end"]),
+                    };
+                }
+                return null;
+            }
+        }
+
+        public void UpdateActivity(ActivityModel activity)
+        {
+            string[] properties = { "title", "description", "date_end" };
+            string[] values = { activity.Title, activity.Description, activity.Date_end.ToString() };
+            string[] where = { "users_idusers = " + activity.Id_user.ToString() };
+            MySqlCommand cmd = _CRUDDAO.Update("tasks", properties, values, where);
+        }
+
+        public void DeleteActivity(int idActivity)
+        {
+            string[] where = { "idtasks = " + idActivity.ToString() };
+            MySqlCommand cmd = _CRUDDAO.Delete("tasks", where);
         }
     }
 }
