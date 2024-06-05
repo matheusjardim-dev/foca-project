3ee4904adeb2a57baf4df132b4c4caef9f3bcb57
matheusjardim-dev <matheusjardim.dev@gmail.com>
2024-06-04 21:30:28 -0300

    CRUD directorys

--- foca-project/Models/Database.cs
+++ foca-project/Models/Database.cs
@@ -9,7 +9,7 @@ namespace foca_project.Models
 {
     public static class Database
     {
-        private const string connectionString = "Server=localhost;Database=foca_db;Uid=root;Pwd=root;";
+        private const string connectionString = "Server=localhost;Database=mydb;Uid=root;Pwd=root;";
 
         public static MySqlConnection GetConnection()
         {
---
+++ foca-project/Models/DirectoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foca_project.Models
+{
+    public class DirectoryModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Id_user { get; set; }
+    }
+}
